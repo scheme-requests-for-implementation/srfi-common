68e36131727e14e3d6ecce1f475e2f1828291e20
Arthur A. Gleckler <srfi@speechcode.com>
2015-06-27 21:18:49 -0700

    Don't hard-code the range of SRFI numbers.

--- admin/srfi-set-up.sh
+++ admin/srfi-set-up.sh
@@ -3,19 +3,23 @@
 SOURCE=~/srfi/split
 TEMP=`mktemp --directory`
 DESTINATION=/var/www/srfi
+
+cd $SOURCE
+
+SRFI=(`find . -maxdepth 1 -type d -regex './srfi-[0-9]+'|awk -F './srfi-' '{print $2'}`)
+
 cd $TEMP/
 for DIR in common email
-  do
-  echo $DIR
-  ((cd $SOURCE/srfi-$DIR && git archive --format=tgz HEAD)|tar xzf -)
+do
+    echo $DIR
+    ((cd $SOURCE/srfi-$DIR && git archive --format=tgz HEAD)|tar xzf -)
 done
 $SOURCE/srfi-common/admin/link-to-new-archives.sh
-for I in $(seq 0 121)
-  do
-  echo srfi-$I
-  (cd $SOURCE/srfi-$I && git archive --format=tgz HEAD)|(cd $TEMP/srfi-$I; tar xzf -)
+for I in ${SRFI[@]}
+do
+    echo srfi-$I
+    (cd $SOURCE/srfi-$I && git archive --format=tgz HEAD)|(cd $TEMP/srfi-$I; tar xzf -)
 done
 cp -p $TEMP/README.html $TEMP/index.html
-
 chmod -R 0755 $TEMP
 mv --force $TEMP $DESTINATION
