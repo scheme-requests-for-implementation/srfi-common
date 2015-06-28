c3617e17acaf958b8bc0c24f8b0a491d6c6e9bbb
Arthur A. Gleckler <srfi@speechcode.com>
2015-06-27 23:18:47 -0700

    Create "srfi.tgz".

--- admin/srfi-set-up.sh
+++ admin/srfi-set-up.sh
@@ -11,17 +11,23 @@ cd $SOURCE
 SRFI=(`find . -maxdepth 1 -type d -regex './srfi-[0-9]+'|awk -F './srfi-' '{print $2'}`)
 
 cd $TEMPDIR_NEW/
+for I in ${SRFI[@]}
+do
+    echo srfi-$I
+    mkdir $TEMPDIR_NEW/srfi-$I
+    (cd $SOURCE/srfi-$I && git archive --format=tgz HEAD)|(cd $TEMPDIR_NEW/srfi-$I; tar xzf -)
+done
+
+TEMPFILE=`mktemp`
+
+tar czf $TEMPFILE .
+mv $TEMPFILE srfi.tgz
 for DIR in common email
 do
     echo $DIR
     ((cd $SOURCE/srfi-$DIR && git archive --format=tgz HEAD)|tar xzf -)
 done
 $SOURCE/srfi-common/admin/link-to-new-archives.sh
-for I in ${SRFI[@]}
-do
-    echo srfi-$I
-    (cd $SOURCE/srfi-$I && git archive --format=tgz HEAD)|(cd $TEMPDIR_NEW/srfi-$I; tar xzf -)
-done
 cp -p $TEMPDIR_NEW/README.html $TEMPDIR_NEW/index.html
 chmod -R 0755 $TEMPDIR_NEW
 mv -T $DESTINATION $TEMPDIR_DEL	  # race condition
