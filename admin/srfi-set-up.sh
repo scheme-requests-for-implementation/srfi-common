9662b1bf9005b249c3cc52025ba33f4a415a1270
Arthur A. Gleckler <srfi@speechcode.com>
2015-06-27 23:10:27 -0700

    Atomic directory move not possible, so shorten race.

--- admin/srfi-set-up.sh
+++ admin/srfi-set-up.sh
@@ -3,13 +3,14 @@
 SOURCE=~/srfi/split
 DESTINATION=/var/www/srfi
 PARENT=`dirname $DESTINATION`
-TEMP=`mktemp --directory --tmpdir=$PARENT`
+TEMPDIR_DEL=`mktemp --directory --tmpdir=$PARENT`
+TEMPDIR_NEW=`mktemp --directory --tmpdir=$PARENT`
 
 cd $SOURCE
 
 SRFI=(`find . -maxdepth 1 -type d -regex './srfi-[0-9]+'|awk -F './srfi-' '{print $2'}`)
 
-cd $TEMP/
+cd $TEMPDIR_NEW/
 for DIR in common email
 do
     echo $DIR
@@ -19,9 +20,10 @@ $SOURCE/srfi-common/admin/link-to-new-archives.sh
 for I in ${SRFI[@]}
 do
     echo srfi-$I
-    (cd $SOURCE/srfi-$I && git archive --format=tgz HEAD)|(cd $TEMP/srfi-$I; tar xzf -)
+    (cd $SOURCE/srfi-$I && git archive --format=tgz HEAD)|(cd $TEMPDIR_NEW/srfi-$I; tar xzf -)
 done
-cp -p $TEMP/README.html $TEMP/index.html
-chmod -R 0755 $TEMP
-cd $PARENT
-mv --force `basename $TEMP` `basename $DESTINATION`
+cp -p $TEMPDIR_NEW/README.html $TEMPDIR_NEW/index.html
+chmod -R 0755 $TEMPDIR_NEW
+mv -T $DESTINATION $TEMPDIR_DEL	  # race condition
+mv $TEMPDIR_NEW $DESTINATION
+rm -rf $TEMPDIR_DEL
