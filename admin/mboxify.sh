9ac597c083ef06b92bef3a5673db1829eb0c9465
Arthur A. Gleckler <srfi@speechcode.com>
2015-07-18 21:16:00 -0700

    Fix bug: only pre-finalization messages were included.
    Also:
    
    * Conditionalize on whether each directory exists.
    
    * Hard-code input directory so that the script can be run from
    anywhere.

--- admin/mboxify.sh
+++ admin/mboxify.sh
@@ -5,8 +5,9 @@ set -e
 # I use a modified version of Eml2mbox that replaces the initial From
 # line rather than adding a new one above the existing one.
 EML2MBOX=/Users/arthur/git-clones/eml2mbox/eml2mbox.rb
+INPUT=~/srfi/srfi-schemers-org/srfi
 OUTPUT=$d
-SRFI=(`find -E . -maxdepth 1 -type d -regex '.*/srfi-[0-9]+'|awk -F './srfi-' '{print $2'}`)
+SRFI=(`cd $INPUT; find -E . -maxdepth 1 -type d -regex '.*/srfi-[0-9]+'|awk -F './srfi-' '{print $2'}`)
 
 cd ~/srfi/split/
 mkdir -p $OUTPUT/mbox/
@@ -16,14 +17,20 @@ do
 
     echo $I
     pushd ~/srfi/srfi-schemers-org/srfi/srfi-$I/archive/
-    for F in `find latest/ -type f`
-    do
-	cp $F $T/original-`basename $F`.eml
-    done
-    for F in `find latest/ -type f`
-    do
-	cp $F $T/latest-`basename $F`.eml
-    done
+    if [ -d latest ]
+    then
+	for F in `find latest/ -type f`
+	do
+	    cp $F $T/latest-`basename $F`.eml
+	done
+    fi
+    if [ -d original ]
+    then
+       for F in `find original/ -type f`
+       do
+	   cp $F $T/original-`basename $F`.eml
+       done
+    fi
     cd $T/
     $EML2MBOX . $OUTPUT/mbox/srfi-$I.mbox
     rm -rf $T/
