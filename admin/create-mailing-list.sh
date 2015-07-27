51c8a80e023e946795188527bc4b6a8e7ead4901
Arthur A. Gleckler <srfi@speechcode.com>
2015-07-27 09:29:49 -0700

    Allow public access to SRFI email archives.

--- admin/create-mailing-list.sh
+++ admin/create-mailing-list.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl \
-  --data "{\"action\":\"create\",\"entity\":\"list\",\"params\":[{\"name\":\"srfi-$NUMBER\",\"archive_enabled\":true,\"archive_spammode\":true,\"moderate\":6,\"restrict_post_lists\":[\"srfi-$NUMBER\",\"srfi-auto-subscribe\"],\"subs_memberview\":\"\"}]}" \
+  --data "{\"action\":\"create\",\"entity\":\"list\",\"params\":[{\"name\":\"srfi-$NUMBER\",\"archive_enabled\":true,\"archive_protected\":false,\"archive_spammode\":true,\"moderate\":6,\"restrict_post_lists\":[\"srfi-$NUMBER\",\"srfi-auto-subscribe\"],\"subs_memberview\":\"\"}]}" \
   --header 'Content-Type: application/json' \
   --header "Authorization: Bearer $TOKEN" \
   --url https://www.simplelists.com/api/api.php
