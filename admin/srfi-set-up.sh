b8209ad15ded083f573ed98a2b0c4dc99b149c8c
Arthur A. Gleckler <srfi@speechcode.com>
2015-08-31 16:15:38 -0700

    Eliminate the need for an overlay.
    I can't believe that I thought I needed an overlay for this.  It's
    probably possible to do this in fewer than three <rsync> steps, but
    this is already so much better, and still fast, that I'm happy.

--- admin/srfi-set-up.sh
+++ admin/srfi-set-up.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# Use an overlay because we run <rsync> twice to copy the contents of
-# all the repositories in place, but we need to use <--delete> to
-# delete obsolete files.  Without an overlay, that would cause the
-# first <rsync>'s files to be deleted.  I'd prefer a less complex way
-# of doing this that was still fast.  Using an overlay has the side
-# benefit of making constructing "srfi.tgz" trivial without having to
-# jump through hoops for exclusions.
-
-# Note that, even in a writeable overlay (i.e. one where a <workdir>
-# is specified), writes to the underlying lower and upper filesystems
-# are not supported while the mount is active.  "If the underlying
-# filesystem is changed, the behavior of the overlay is undefined,
-# though it will not result in a crash or deadlock."  (See
-# <https://goo.gl/fXc9Zq>.)  So I do it anyway, then unmount and
-# re-mount.
-
-# TODO: Rewrite this in Scheme without overlays, combining the <rsync>
-# steps into one.
+# Do this in three <rsync> steps because we need to put only a subset
+# of the files in "srfi.tgz".
 
 SOURCE=~/srfi/split
 DESTINATION=/var/www/srfi
@@ -50,9 +34,9 @@ mv $TEMPFILE $DESTINATION_UPPER/srfi.tgz
 rsync \
     --checksum \
     --delete \
+    --exclude='*~' \
     --exclude='.gitignore' \
     --exclude='.git/' \
-    --exclude='*~' \
     --exclude='admin/' \
     --progress \
     --recursive \
@@ -62,7 +46,13 @@ rsync \
     $SOURCE/srfi-email/* \
     $DESTINATION_UPPER/
 cp --force -p $DESTINATION_UPPER/README.html $DESTINATION_UPPER/index.html
-chmod -R 0755 $DESTINATION_UPPER
-chmod -R 0755 $DESTINATION_LOWER
-echo "Don't forget to unmount and re-mount $DESTINATION."
-echo "Use <sudo srfi-remount.sh> after checking its contents." 
+rsync \
+    --checksum \
+    --delete \
+    --progress \
+    --recursive \
+    --times \
+    /var/www/srfi-lower/ \
+    /var/www/srfi-upper/ \
+    $DESTINATION/
+chmod -R 0755 $DESTINATION/
