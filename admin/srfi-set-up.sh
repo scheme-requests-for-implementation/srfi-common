91d947256c5cf6478a6f99a287b93661934c2e57
Arthur A. Gleckler <srfi@speechcode.com>
2015-08-31 16:16:14 -0700

    Reduce indentation.

--- admin/srfi-set-up.sh
+++ admin/srfi-set-up.sh
@@ -13,46 +13,46 @@ mkdir -p $DESTINATION_UPPER
 mkdir -p $DESTINATION_LOWER
 
 rsync \
-    --checksum \
-    --delete \
-    --exclude='*~' \
-    --exclude='.gitignore' \
-    --exclude='.git/' \
-    --exclude='srfi-common/' \
-    --exclude='srfi-email/' \
-    --progress \
-    --recursive \
-    --safe-links \
-    --times \
-    $SOURCE/ \
-    $DESTINATION_LOWER/
+  --checksum \
+  --delete \
+  --exclude='*~' \
+  --exclude='.gitignore' \
+  --exclude='.git/' \
+  --exclude='srfi-common/' \
+  --exclude='srfi-email/' \
+  --progress \
+  --recursive \
+  --safe-links \
+  --times \
+  $SOURCE/ \
+  $DESTINATION_LOWER/
 
 TEMPFILE=`mktemp`
 
 (cd $DESTINATION_LOWER/; tar czf $TEMPFILE --exclude=srfi.tgz .)
 mv $TEMPFILE $DESTINATION_UPPER/srfi.tgz
 rsync \
-    --checksum \
-    --delete \
-    --exclude='*~' \
-    --exclude='.gitignore' \
-    --exclude='.git/' \
-    --exclude='admin/' \
-    --progress \
-    --recursive \
-    --safe-links \
-    --times \
-    $SOURCE/srfi-common/* \
-    $SOURCE/srfi-email/* \
-    $DESTINATION_UPPER/
+  --checksum \
+  --delete \
+  --exclude='*~' \
+  --exclude='.gitignore' \
+  --exclude='.git/' \
+  --exclude='admin/' \
+  --progress \
+  --recursive \
+  --safe-links \
+  --times \
+  $SOURCE/srfi-common/* \
+  $SOURCE/srfi-email/* \
+  $DESTINATION_UPPER/
 cp --force -p $DESTINATION_UPPER/README.html $DESTINATION_UPPER/index.html
 rsync \
-    --checksum \
-    --delete \
-    --progress \
-    --recursive \
-    --times \
-    /var/www/srfi-lower/ \
-    /var/www/srfi-upper/ \
-    $DESTINATION/
+  --checksum \
+  --delete \
+  --progress \
+  --recursive \
+  --times \
+  /var/www/srfi-lower/ \
+  /var/www/srfi-upper/ \
+  $DESTINATION/
 chmod -R 0755 $DESTINATION/
