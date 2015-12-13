8f1d8e14209538efe99c28ee794fd4125d08a18f
Arthur A. Gleckler <srfi@speechcode.com>
2015-12-13 10:22:51 -0800

    Rename SRFI 130.  Remove Will Clinger as co-author.

--- admin/srfis.scm
+++ admin/srfis.scm
@@ -142,7 +142,7 @@
     (127 draft "Lazy Sequences" "John Cowan")
     (128 draft "Comparators (reduced)" "John Cowan")
     (129 draft "Titlecase procedures" "John Cowan")
-    (130 draft "Character spans" "John Cowan, Will Clinger")
+    (130 draft "String cursors" "John Cowan")
     (131 draft "ERR5RS Record Syntax (reduced)" "John Cowan, Will Clinger")
     (132 draft "Sort Libraries" "Olin Shivers (edited by John Cowan)")))
 
