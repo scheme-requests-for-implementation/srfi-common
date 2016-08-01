9a27752cee6635a89802a9bdab93c7cd349ff50f
Arthur A. Gleckler <srfi@speechcode.com>
2016-07-31 17:54:59 -0700

    Rename SRFI 140 to Immutable Strings.
    It was originally called Strings for WG2.

--- admin/srfis.scm
+++ admin/srfis.scm
@@ -152,7 +152,7 @@
     (137 draft "Minimal Unique Types" "John Cowan, Marc Nieper-Wißkirchen")
     (138 draft "Compiling Scheme programs to executables" "Marc Nieper-Wißkirchen")
     (139 draft "Syntax parameters" "Marc Nieper-Wißkirchen")
-    (140 draft "Strings for WG2" "Per Bothner")))
+    (140 draft "Immutable Strings" "Per Bothner")))
 
 ;; This is the first SRFI whose email archive was hosted first on
 ;; Simplelists.  SRFI with smaller numbers were originally hosted on
