e0d9d012b467b8a270617b9210d53ba34edf706a
Arthur A. Gleckler <srfi@speechcode.com>
2017-08-17 18:23:23 -1000

    Sort by number when primary keys equal.

--- list.js
+++ list.js
@@ -1,4 +1,20 @@
 var options = {
+  sortFunction: function(itemA, itemB, options) {
+    let column = options.valueName;
+    let sort = srfiList.utils.naturalSort;
+    let primary = sort(itemA.values()[column],
+                       itemB.values()[column]);
+
+    if (primary == 0) {
+      if (column === "number") {
+        return 0;
+      }
+      return sort(itemA.values()["number"],
+                  itemB.values()["number"]);
+    }
+    return primary;
+
+  },
   valueNames: [ "authors",
                 "date",
                 "name",
