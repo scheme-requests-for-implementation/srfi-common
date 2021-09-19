e5c4ddb7b216eda2a73fd562d9e91d6b4cebda41
Arthur A. Gleckler <srfi@speechcode.com>
2021-09-18 20:19:24 -0700

    Add signatures for SRFI 140.
    Automate the process of expanding (foo [start end]), for example,
    to (foo), (foo start), and (foo start end).

---
+++ index/expand-signature.scm
@@ -0,0 +1,63 @@
+;;;; Expand signatures
+
+;; Often, macro and procedure signatures in SRFIs are expressed like this:
+;;
+;;   (string-every pred string [start end])
+;;
+;; This usually means that either `start', or `start' and `end', or neither can
+;; be supplied as arguments.  I want to expand the above notation to the
+;; following notation, which is unambiguous:
+
+;;   (string-every pred string)
+;;   (string-every pred string start)
+;;   (string-every pred string start end)
+
+;; The procedure `expand-signature', below, performs that expansion given a
+;; slightly different input form:
+
+;;   (string-every pred string #(start end))
+
+;; Note that this assumes that closing never appear anywhere except at the end
+;; of the containing list.  It also assumes that [foo bar] has the same meaning
+;; as [foo [bar]].  (Sometimes, people define [foo bar] to mean that either both
+;; symbols or neither must appear.)
+
+(define (expand-signature x)
+  (cond ((null? x) '(()))
+	((pair? x)
+	 (cond ((vector? (car x))
+		(assert (null? (cdr x)))
+		(let next-tail ((y (vector->list (car x))))
+		  (if (null? y)
+		      '(())
+		      (cons '()
+			    (let ((heads (expand-signature (car y))))
+			      (append-map (lambda (t)
+					    (map (lambda (h) (cons h t))
+						 heads))
+					  (next-tail (cdr y))))))))
+	       (else
+		(append-map (lambda (h)
+			      (map (lambda (t) (cons h t))
+				   (expand-signature (cdr x))))
+			    (expand-signature (car x))))))
+	((vector? x) (vector->list x))
+	(else (list x))))
+
+(define (expand-index-entry e)
+  (append-map (lambda (x)
+		(if (and (pair? x) (eq? (car x) 'signature))
+		    (expand-signature x)
+		    (list x)))
+	      e))
+
+;; for example:
+
+;; (with-input-from-file "/tmp/srfi-140.signatures.scm"
+;;   (lambda ()
+;;     (let next-signature ((accumulator '()))
+;;	 (let ((input (read)))
+;;	   (if (eof-object? input)
+;;	       (for-each pp (reverse accumulator))
+;;	       (next-signature
+;;		(cons (expand-index-entry input) accumulator)))))))
