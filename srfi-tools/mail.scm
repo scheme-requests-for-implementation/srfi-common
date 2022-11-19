d0b0972aa991757a2d0b4e5801cbfde006a55375
Arthur A. Gleckler <srfi@speechcode.com>
2022-11-18 22:56:19 -0800

    Factor body of "mail.sld" into "mail.scm".

---
+++ srfi-tools/mail.scm
@@ -0,0 +1,100 @@
+(define (srfi-mail-archive-url num)
+  (string-append "https://srfi-email.schemers.org/"
+                 (srfi-num-stem num) "/"))
+
+(define-command (mail-archive-url num)
+  "Display mail archive URL for SRFI <num>."
+  (write-line-about-srfi srfi-mail-archive-url num))
+
+(define (srfi-mail-address num)
+  (string-append (srfi-num-stem num)
+                 "@srfi.schemers.org"))
+
+(define-command (mail-address num)
+  "Display email address URL for SRFI <num>."
+  (write-line-about-srfi srfi-mail-address num))
+
+(define (mailto-address-subject address subject)
+  (string-append "mailto:"
+                 address
+                 "?subject=" (url-hexify-string subject)))
+
+(define (srfi-mailto-url num)
+  (mailto-address-subject (srfi-mail-address num)
+                          (srfi-title num)))
+
+(define (cap-first string)
+  (if (zero? (string-length string)) ""
+      (string-append (string-upcase (string-copy string 0 1))
+                     (string-copy string 1 (string-length string)))))
+
+(define (srfi-last-call num author-name-part)
+  (let* ((srfi (srfi-by-number num))
+         (email (srfi-mail-address num))
+         (url (srfi-landing-url num))
+         (num (number->string num))
+         (title (srfi-title srfi))
+         (author (srfi-author-name
+                  (or (find (lambda (author)
+                              (string-contains
+                               (string-downcase
+                                (srfi-author-name author))
+                               (string-downcase
+                                author-name-part)))
+                            (srfi-authors srfi))
+                      (error "Cannot find author."))))
+         (author/co-author
+          (if (= 1 (length (srfi-authors srfi)))
+              "author"
+              "co-author"))
+         (pronoun "he")
+         (date (date->iso-date
+                (julian-day->date
+                 (+ 7
+                    (truncate  ; Work around bug in Chibi's SRFI 19.
+                     (date->julian-day (current-date)))))))
+         (subject
+          (string-append
+           "Last call for comments on SRFI " num ": " title)))
+    (string-join-lines
+     (map
+      string-concatenate
+      `((,(mailto-address-subject email subject))
+        ("")
+        ("Subject: " ,subject)
+        ("")
+        (,author ", " ,author/co-author " of [[" ,url "][SRFI")
+        (,num "]]: " ,title ", has asked me to announce *last call*"
+              " for this SRFI.  " ,(cap-first pronoun))
+        ("believes that it is ready for finalization,"
+         " but would like to give")
+        ("reviewers one last chance to submit corrections and"
+         " feedback before we")
+        ("finalize it.")
+        ("")
+        ("In particular, I appeal to anyone reading this to try"
+         " the sample")
+        ("implementation, run the tests, and send feedback about"
+         " your results.")
+        ("")
+        ("If you're interested in this SRFI, please give your"
+         " feedback via the")
+        ("SRFI " ,num " mailing list before *" ,date "*.  After that,"
+         " assuming that no")
+        ("major revisions are required, we will declare it"
+         " final.  It is")
+        ("important that we get your feedback before " ,date
+         ".  *If that deadline")
+        ("is too soon for you, but you would like"
+         " to contribute, please let me know")
+        ("so that I can extend the last-call period.*")
+        ("")
+        ("Regards,")
+        ("")
+        ("")
+        ("SRFI Editor"))))))
+
+(define-command (last-call num author-name-part)
+  "Display HTML of last-call message for SRFI <num> with author <autho-name-part>."
+  (write-string (srfi-last-call (parse-srfi-number num)
+                                author-name-part)))
